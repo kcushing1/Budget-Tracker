a6fdda07d12905cd93ec5aeaa07a516bb639769b
Kasey Cushing <kasey.cushing@gmail.com>
2021-02-08 16:18:01 -0500

    webpack.config.js (without webpack-pwa-manifest) in /public

---
+++ public/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const config = {
+  entry: "/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
